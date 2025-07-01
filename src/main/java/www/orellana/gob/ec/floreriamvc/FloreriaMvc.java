f2dd111e6f43030f2569fd8287a4974a5e5f61d8
Stalyn0101 <andreaquiroga010@gmail.com>
2025-06-30 20:40:05 -0500

    GestionFloreria

---
+++ src/main/java/www/orellana/gob/ec/floreriamvc/FloreriaMvc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package www.orellana.gob.ec.floreriamvc;
+
+/**
+ *
+ * @author USER
+ */
+public class FloreriaMvc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
